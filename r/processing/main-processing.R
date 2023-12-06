5ab3a9b18c6366351592d65fb31d014e8bc64bf4
Ville Inkinen <ville.inkinen@hotmail.com>
2023-12-06 16:24:12 +0000

    Update main-processing.R

--- r/processing/main-processing.R
+++ r/processing/main-processing.R
@@ -1,7 +1,5 @@
 
 
-
-
 # Forest gain and loss layers
 source(paste0(project_path, "r/processing/process-forest-gain.R"), local = new.env())
 source(paste0(project_path, "r/processing/process-forest-loss.R"), local = new.env())
@@ -9,9 +7,5 @@ source(paste0(project_path, "r/processing/process-forest-loss.R"), local = new.e
 
 # Forest gain and loss survival length layers
 source(paste0(project_path, "r/processing/process-slen.R"), local = new.env())
-
-
-
-
 
 
