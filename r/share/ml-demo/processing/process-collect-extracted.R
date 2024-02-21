a06e946a6cee6fe42c1519222d1dda68ead803dc
Ville Inkinen <ville.inkinen@hotmail.com>
2024-02-21 09:32:37 +0000

    Update process-collect-extracted.R

--- r/share/ml-demo/processing/process-collect-extracted.R
+++ r/share/ml-demo/processing/process-collect-extracted.R
@@ -3,11 +3,9 @@
 # Read data
 
 dt <- fread(filenames$ml_demo$master_dt, colClasses = c(cell = "numeric"))
+e_list <- lapply(list.files(paste0(project_path, "data/constructed/csv/ml-demo-extracted/"), recursive = TRUE, full.names = TRUE), fread, colClasses = c(cell = "numeric"))
 
-in_folder <- paste0(project_path, "data/constructed/csv/ml-demo-extracted/")
-
-e_list <- lapply(list.files(in_folder, recursive = TRUE, full.names = TRUE), fread, colClasses = c(cell = "numeric"))
-
+# Join extracted data to master
 
 for (i in 1:length(e_list)) {
 
@@ -27,6 +25,7 @@ for (i in 1:length(e_list)) {
 
 }
 
+# Save
 
 dt |> fwrite(filenames$ml_demo$master_dt)
 
